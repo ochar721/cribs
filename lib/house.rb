19cf6b8491fc7cddc02f511a9c646671a4ff12bb
Olivia Charles <ocharles92@gmail.com>
2020-12-09 16:57:23 -0500

    Writing Rooms from Category

--- lib/house.rb
+++ lib/house.rb
@@ -15,7 +15,13 @@ class House
     @rooms << room
   end
 
+  def above_market_average?
+    if price < 500000
+      false
+    end
+  end
 
-
-
+  def rooms_from_category(room)
+    :room.fetch(category)
+  end
 end
